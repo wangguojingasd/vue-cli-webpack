d162481e121fe469d403abe1ed1ae8aa109b43ed
王国靖 <wgjsxc@gmail.com>
2019-01-31 09:48:12 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,29 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
-    root: true,
-    // 解析器，这里我们使用babel-eslint
-    parser: 'babel-eslint',
-    parserOptions: {
-      sourceType: 'module'
-    },
-    // required to lint *.vue files
-    plugins: [
-      'html'
-    ],
-    // add your custom rules here
-    'rules': {
-      // allow debugger during development
-      'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-    }
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential', 
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
+}
